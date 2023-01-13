3c2a56414597143000486c62d40edca9f9871160
bl41s3 <global-config@example.com>
2023-01-12 19:49:17 -0500

    segundo commit del proyecto

--- payment/main.py
+++ payment/main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.background import BackgroundTasks
 from redis_om import get_redis_connection, HashModel
 from starlette.requests import Request
-import requests
+import requests, time
 
 app = FastAPI()
 
@@ -32,10 +33,29 @@ class Order(HashModel):
     class Meta:
         database = redis
 
-@app.get('/orders')
-async def create(request: Request):
+@app.post('/orders')
+async def create(request: Request, background_tasks: BackgroundTasks):   # id, quantity
     body = await request.json()
 
     req = requests.get('http://localhost:8000/products/%s' % body['id'])
+    product = req.json()
 
-    return req.json()
+    order = Order(
+        product_id=body['id'],
+        price=product['price'],
+        fee=0.2 * product['price'],
+        total=1.2 * product['price'],
+        quantity=body['quantity'],
+        status='pending'
+    )
+    order.save()
+
+    order_completed(order)
+
+    return order
+
+
+def order_completed(order: Order):
+    order.status = 'completed'
+    order.save()
+
